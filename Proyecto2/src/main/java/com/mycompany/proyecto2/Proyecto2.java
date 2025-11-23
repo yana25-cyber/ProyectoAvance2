4405593c7ff885c3102439d5e15cb191c4d827d7
yana25-cyber <correo>
2025-11-22 23:57:27 -0600

    Base de mi proyecto

---
+++ Proyecto2/src/main/java/com/mycompany/proyecto2/Proyecto2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto2;
+
+/**
+ *
+ * @author yana1
+ */
+public class Proyecto2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
