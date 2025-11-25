b73205ceeaa3792758858e16f8549d2bad9936c8
Johansen Sánchez <jsanchez60600@ufide.ac.cr>
2025-11-25 17:42:59 -0600

    Add files via upload

---
+++ RegistrarEmplead01/src/registraremplead01/MenuEmpleado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package registraremplead01;
+
+/**
+ *
+ * @author johan
+ */
+public class MenuEmpleado {
+    
+}
